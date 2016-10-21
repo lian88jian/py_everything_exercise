0b763d7de7cc7901a69c929ba44bc786b5fc9860
lianjian <lianjian@goalsword>
2016-10-21 13:12:21 +0800

    1

--- src/pack_2016_10_20_wechat_itchat/wechat_robot.py
+++ src/pack_2016_10_20_wechat_itchat/wechat_robot.py
@@ -17,18 +17,20 @@ def text_reply(msg):
     
     print('<< %s : %s' % (fromUserName, msgContent)) 
     
-    if msgContent.lower() == dog_name + ' away':
+    if msgContent.lower() == dog_name.lower() + ' come':
+        friend['away'] = False;
+        reply = '(ฅ´ω`ฅ) %s来也! 作为一只怪兽，我的愿望是消灭一坨奥特曼!' % dog_name;
+    elif 'away' in friend and friend['away'] == True:
+        return "";
+    elif msgContent.lower() == dog_name.lower() + ' away':
         friend['away'] = True;
         reply = '~~呜~~...╥﹏╥...输入%s come重新召唤' % dog_name;
-    elif msgContent.lower() == dog_name + ' come':
-        friend['away'] = False;
-        reply = '作为一只怪兽，我的愿望是至少消灭一坨奥特曼!';
     else:
         if 'isFirst' not in friend: #是第一次
-            reply = "(ฅ´ω`ฅ)我是贱贱的狗狗 %s,贱贱现在不在,我可以陪你聊会儿!输入%s away我将消失" % (dog_name, dog_name)
-            friend['isFirst'] = False;
+            reply = "(ฅ´ω`ฅ)我是%s的狗狗 %s,主人现在不在,我可以陪你聊会儿!输入%s away我将消失" % (master_name, dog_name, dog_name)
         else:
             reply = get_response(msgContent)
+    friend['isFirst'] = False;
     print(">> %s: %s" % (dog_name, reply) );
     return "%s: %s" % (dog_name, reply);
 
@@ -37,8 +39,7 @@ except_users = None;
 def get_friend(userName):
     for f in friends:
         if f['UserName'] == userName:
-            f['away'] = False;
-            if f['NickName'] in except_users:
+            if f['NickName'] in except_users or f['RemarkName'] in except_users:
                 return None;
             return f;
     return None;
@@ -47,16 +48,19 @@ def get_friend(userName):
 if __name__ == '__main__':
     try:
         with open('config.json') as f: 
-            except_users = json.loads(f.read())['except_users']
-            dog_name = json.loads(f.read())['dog_name'];
-            QR = json.loads(f.read())['QR'];
+            config_json = json.loads(f.read());
+            except_users = config_json['except_users'];
+            dog_name = config_json['dog_name'];
+            QR = config_json['QR'];
+            master_name = config_json['master_name'];
         print("load config file success!")
     except:
         except_users = [] #不用机器人的列表
         dog_name = 'noob';
+        master_name = '贱贱';
         QR = -2;
         
-    itchat.auto_login(True, enableCmdQR = False)
+    itchat.auto_login(True, enableCmdQR = QR)
     friends = itchat.get_friends(True);
     except_users.append(friends[0]['NickName'])
     threading.Thread(target=itchat.run).start();
